7ba19e036866ac6cf9757e2f4e01f0a00cefad8b
Nekromenzer <bamaneeshagimshan1999@gmail.com>
2023-10-22 20:14:37 +0530

    update links

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,25 @@ import {
   BsLinkedin,
   BsGithub,
   BsYoutube,
-  BsTiktok
+  BsTiktok,
+  BsWhatsapp,
+  BiLogoTelegram
 } from 'react-icons/bs'
 
 function App () {
   const socialLinks = [
+    {
+      name: 'Telegram Group',
+      url: 'https://t.me/+0AQ16PogUEM1YThl',
+      icon: <BiLogoTelegram className='text-2xl group-hover:text-3xl' />,
+      customClass: 'bg-[#229ED9] text-white'
+    },
+    {
+      name: 'Whatsapp channel',
+      url: 'https://whatsapp.com/channel/0029VaDKBYI5Ui2cPup58U40',
+      icon: <BsWhatsapp className='text-2xl group-hover:text-3xl' />,
+      customClass: 'bg-[#128C7E] text-white'
+    },
     {
       name: 'Github',
       url: 'https://github.com/Nekromenzer',
@@ -43,14 +57,9 @@ function App () {
   return (
     <div className='flex flex-col justify-center items-center gap-6 my-8'>
       {socialLinks.map((item, idx) => (
-        <a
-          href={item.url}
-          target='_blank'
-          rel='noreferrer'
-          key={idx}
-        >
+        <a href={item.url} target='_blank' rel='noreferrer' key={idx}>
           <div
-            className={`${item.customClass} rounded-3xl shadow  hover:px-12 gap-12 min-w-[18rem] duration-500 transition-all group w-auto flex items-center justify-between px-8 py-1 min-h-[3rem] max-w-[30rem]`}
+            className={`${item.customClass} rounded-3xl shadow  hover:px-12 gap-12 min-w-[20rem] duration-500 transition-all group w-auto flex items-center justify-between px-8 py-1 min-h-[3rem] max-w-[30rem]`}
           >
             <div className='font-mono text-xl'>{item.name}</div>
             <div>{item.icon}</div>
